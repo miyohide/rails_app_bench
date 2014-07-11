97c6589128686d0cb30e31a1445ede7cc782fefe
Hidenori Miyoshi <miyohide@gmail.com>
2014-07-11 22:05:13 +0900

    create tag table and tag model.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,3 @@
 class Entry < ActiveRecord::Base
+   has_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628102153) do
+ActiveRecord::Schema.define(version: 20140711125342) do
 
   create_table "entries", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "tag_name"
+    t.integer  "entry_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
