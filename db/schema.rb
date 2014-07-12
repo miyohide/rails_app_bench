4614b5f154b8e46984832b40ee3087d4cc857b4d
Hidenori Miyoshi <miyohide@gmail.com>
2014-07-12 20:18:11 +0900

    タグの持ち方を変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711125342) do
+ActiveRecord::Schema.define(version: 20140712103355) do
 
   create_table "entries", force: true do |t|
     t.string   "title"
@@ -20,9 +20,18 @@ ActiveRecord::Schema.define(version: 20140711125342) do
     t.datetime "updated_at"
   end
 
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "entry_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["entry_id"], name: "index_taggings_on_entry_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
   create_table "tags", force: true do |t|
     t.string   "tag_name"
-    t.integer  "entry_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
